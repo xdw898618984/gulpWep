50c2692be2c98ff471c34336eaf717b961ae3c6d
xdw <898618984@qq.com>
2019-05-16 22:06:53 +0800

    模板式表单

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+  public myform;
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSubmit(value,event) {
+    this.myform = value;
+    console.log(this.myform);
+  }
+  // submit() {
+  //   console.log(this.myform);
+  // }
 }
