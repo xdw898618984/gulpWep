ccc2b91fcadeefd01ad3ecb39884f7815856c48b
xdw898618984 <898618984@qq.com>
2019-05-23 10:58:36 +0800

    重构响应式

--- src/app/form/profileeditor/profileeditor.component.ts
+++ src/app/form/profileeditor/profileeditor.component.ts
@@ -1,36 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from "@angular/forms"
-@Component({
-  selector: 'app-profileeditor',
-  templateUrl: './profileeditor.component.html',
-  styleUrls: ['./profileeditor.component.css']
-})
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
+// valistaors 常用验证器函数
+@Component({ selector: 'app-profileeditor', templateUrl: './profileeditor.component.html', styleUrls: ['./profileeditor.component.css'] })
 export class ProfileeditorComponent implements OnInit {
 
-  constructor() { }
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
+  constructor(private fb: FormBuilder) { }
+  // profileForm = new FormGroup({
+  //   // firstName: new FormControl(''),
+  //   // lastName: new FormControl(''),
+  //   // address: new FormGroup({ street: new FormControl(''), city: new
+  //  FormControl(''), state: new FormControl(''), zip: new FormControl('') })
 
+
+
+  // });
+
+
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
     })
-  })
-  ngOnInit() {
-  }
+  });
+  ngOnInit() { }
 
   onSubmit() {
     console.warn(this.profileForm);
 
   }
   updateProfile() {
-    this.profileForm.patchValue({
-      firstName: '熊德伟', address: {
-        street:'123 dw'
-      }
-    })
+    this
+      .profileForm
+      .patchValue({
+        firstName: '熊德伟',
+        address: {
+          street: '123 dw'
+        }
+      });
   }
 }
--- src/app/units/debug.ts
+++ src/app/units/debug.ts
@@ -1,29 +1,26 @@
-import { Observable } from 'rxjs'
+import {Observable} from 'rxjs';
 
 declare module 'rxjs' {
-    interface Observable<T> {
-        debug: (...any) => Observable<T>;
+    interface Observable < T > {
+        debug: (...any) => Observable < T >;
     }
 }
 
 const debuggerOn = true;
 
-Observable.prototype.debug = function (message: string) {
-    return this.do(
-        nextValue => {
+Observable.prototype.debug = function (message : string) {
+    return this.do 
+        (nextValue => {
             if (debuggerOn) {
                 console.log(message, nextValue);
             }
-        },
-        err => {
+        }, err => {
             if (debuggerOn) {
                 console.error(message, err);
             }
-        },
-        () => {
+        }, () => {
             if (debuggerOn) {
                 console.log('Observable completed - ' + message);
             }
-        }
-    );
-};
+        });
+    };
