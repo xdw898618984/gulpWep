b1f042001bd20ce868ebec4adb54429d77ae413c
xdw898618984 <898618984@qq.com>
2019-06-14 14:41:25 +0800

    angular7拖拽特性

--- src/app/common/test4/test4.component.ts
+++ src/app/common/test4/test4.component.ts
@@ -1,26 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Product } from '../test1/test1.component';
 
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({ selector: 'app-test4', templateUrl: './test4.component.html', styleUrls: ['./test4.component.css'] })
 export class Test4Component implements OnInit {
 
-  customers = [
-    { name: 'Adam', age: 23 },
-    { name: 'Jack', age: 27 },
-    { name: 'Katherin', age: 26 },
-    { name: 'John', age: 30 },
-    { name: 'Watson', age: 42 },
-  ]; constructor(private route: ActivatedRoute) { }
+  // customers = [
+  //   { name: 'Adam', age: 23 },
+  //   { name: 'Jack', age: 27 },
+  //   { name: 'Katherin', age: 26 },
+  //   { name: 'John', age: 30 },
+  //   { name: 'Watson', age: 42 },
+  // ];
+
+  customers = [new Customer('Adam', 12), new Customer('Jack', 18), new Customer('John', 12), new Customer('Watson', 18)]
+  customers3 = [new Customer('Adam', 12), new Customer('Jack', 18), new Customer('John', 12), new Customer('Watson', 18)]
+  customers2 = [new Customer('昌亚欣', 12), new Customer('熊德伟', 18)]
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() { }
-
   drop(event: CdkDragDrop<string[]>) {
-    console.log('列表排序：', event, this.customers);
-    console.log('列表排序：', event, this.customers);
-    moveItemInArray(this.customers, event.previousIndex, event.currentIndex);
+    console.log(event);
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex)
+    }
+    console.log(this.customers2);
+
+  }
+  drops(event: CdkDragDrop<string[]>) {
+    console.log(this.customers);
+    console.log(event);
+    moveItemInArray(this.customers3, event.previousIndex, event.currentIndex);
   }
 
 }
+class Customer {
+  constructor(private name: string, private age: number) {
+
+  }
+};
